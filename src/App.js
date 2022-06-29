00a85646e3b71d675310899ce530187a0894513c
mespinozar <rieraes@gmail.com>
2022-06-28 21:34:23 -0500

    Avance Componentes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
-import { Product } from './components/Product';
+
 import { DetailProduct } from './components/DetailProduct';
 import { Routes, Route, BrowserRouter as Router} from 'react-router-dom'
 
@@ -10,7 +10,6 @@ export const App = () => {
   return (
     <Router>
       <Header/>
-      <Product/>
       <Routes>
         <Route path="/" element = { <Home /> } />
         <Route path="product/:id" element={ <DetailProduct/> } />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Form } from '../components/Form'
+import { Product } from '../components/Product'
 
 export const Home = () => {
   return (
     <div>
-        <Form />
+        <Product />
     </div>
   )
 }
